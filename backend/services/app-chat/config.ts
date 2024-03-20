a917970939b6b76ea18d19a43596936d4aad576e
sieusaya149 <hungdevngu99@gmail.com>
2024-03-20 16:27:57 +0700

    A36 impro: clean code

--- backend/services/app-chat/config.ts
+++ backend/services/app-chat/config.ts
@@ -1,22 +1,10 @@
 import path from 'path';
-import dotenv from 'dotenv';
 
-dotenv.config(); // Load environment variables from .env file
-// Mapper for environment variables
 export const environment = process.env.NODE_ENV;
-export const port = process.env.PORT;
-export const timezone = process.env.TZ;
-// Allow crossing sharing for domain
-export const corsUrl = process.env.CORS_URL || '*';
 
-export const tokenInfo = {
-    accessTokenValidity: parseInt(process.env.ACCESS_TOKEN_VALIDITY_SEC || '0'),
-    refreshTokenValidity: parseInt(
-        process.env.REFRESH_TOKEN_VALIDITY_SEC || '0'
-    ),
-    issuer: process.env.TOKEN_ISSUER || '',
-    audience: process.env.TOKEN_AUDIENCE || ''
-};
+export const port = process.env.CHAT_PORT;
+
+export const corsUrl = process.env.CORS_URL || '*';
 
 export const logDirectory = process.env.LOG_DIR;
 
--- backend/services/app-user/server.ts
+++ backend/services/app-user/server.ts
@@ -1,8 +1,7 @@
 import Logger from '@viethung/logger';
 import {port} from './config';
 import app from './app';
-import dotenv from 'dotenv';
-dotenv.config();
+
 app.listen(port, () => {
     console.log(`server running on port : ${port}`);
 }).on('error', (e: Error) => Logger.error(e));
